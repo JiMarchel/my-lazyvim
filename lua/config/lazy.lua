6b5d96876503ce18d677009c31ff771764bd8493
marchel <jmarchel100@gmail.com>
2024-12-16 18:49:13 +0700

    plugins and colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,7 +18,13 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.sql" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.prisma" },
     { import = "plugins" },
   },
   defaults = {
